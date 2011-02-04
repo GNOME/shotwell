0e6b31b82553cf5f177d5ce8f0071b2ffc94d758
Jim Nelson <jim@yorba.org>
2011-02-04 23:43:36 +0000

    Build system now removes temp file leftover during VAPI creation.

--- src/plugins/mk/interfaces.mk
+++ src/plugins/mk/interfaces.mk
@@ -14,6 +14,7 @@ plugins/shotwell-spit-1.0.deps: plugins/shotwell-spit-1.0.vapi
 plugins/shotwell-spit-1.0.vapi: src/plugins/SpitInterfaces.vala
 	$(call check_valac_version)
 	$(VALAC) -c $(VALAFLAGS) -X -I. --includedir=plugins --vapi=$@ --header=$(basename $@).h $<
+	rm $(notdir $<).o
 
 plugins/shotwell-transitions-1.0.deps: plugins/shotwell-transitions-1.0.vapi
 	@printf "gdk-2.0\nshotwell-spit-1.0\n" > $@
@@ -21,4 +22,4 @@ plugins/shotwell-transitions-1.0.deps: plugins/shotwell-transitions-1.0.vapi
 plugins/shotwell-transitions-1.0.vapi: src/plugins/TransitionsInterfaces.vala plugins/shotwell-spit-1.0.vapi
 	$(call check_valac_version)
 	$(VALAC) -c $(VALAFLAGS) -X -I. --pkg=gdk-2.0 --pkg=shotwell-spit-1.0 --includedir=plugins --vapi=$@ --header=$(basename $@).h $<
-
+	rm $(notdir $<).o
