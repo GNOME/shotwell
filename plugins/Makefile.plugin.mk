449ee80f73b74793057c379278bda38bc0b12b50
Jim Nelson <jim@yorba.org>
2011-03-22 18:53:22 +0000

    Took out some Makefile debug code.

--- plugins/Makefile.plugin.mk
+++ plugins/Makefile.plugin.mk
@@ -38,7 +38,6 @@ DEFINES := -D_VERSION='"$(PLUGINS_VERSION)"' -DGETTEXT_PACKAGE='"shotwell"'
 all: $(PLUGIN).so
 
 .stamp: $(SRC_FILES) $(MAKE_FILES) $(HEADER_FILES)
-	echo plugin $(USER_VALAFLAGS)
 	$(VALAC) -g --enable-checking --fatal-warnings --save-temps --compile \
 		--vapidir=../ $(foreach pkg,$(PKGS),--pkg=$(pkg)) \
 		-X -I../.. -X -fPIC \
