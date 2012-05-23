1835f5040cbcc553d5714b0800ede7baa6c38dcb
Clint Rogers <clinton@yorba.org>
2012-05-23 11:23:26 -0700

    Adds some files that get left out when running make dist; closes #5290 and #5293.

--- plugins/plugins.mk
+++ plugins/plugins.mk
@@ -6,8 +6,11 @@ PLUGINS := \
 
 PLUGINS_RC := \
 	plugins/shotwell-publishing/facebook.png \
+	plugins/shotwell-publishing/facebook_publishing_options_pane.glade \
 	plugins/shotwell-publishing/flickr.png \
+	plugins/shotwell-publishing/flickr_publishing_options_pane.glade \
 	plugins/shotwell-publishing/picasa.png \
+	plugins/shotwell-publishing/picasa_publishing_options_pane.glade \
 	plugins/shotwell-publishing/youtube.png \
 	plugins/shotwell-publishing/piwigo.png \
 	plugins/shotwell-publishing/piwigo_authentication_pane.glade \
