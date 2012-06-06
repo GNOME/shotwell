b24079554cb4958e7a2062e096efe08728c367e1
Michael Axiak <mike@axiak.net>
2012-06-06 10:33:32 -0700

    Adds references to two .glade files to the plugins Makefile that were mistakenly omitted during the migration of the publishing UI to Glade. Closes #5350.

--- plugins/plugins.mk
+++ plugins/plugins.mk
@@ -9,9 +9,11 @@ PLUGINS_RC := \
 	plugins/shotwell-publishing/facebook_publishing_options_pane.glade \
 	plugins/shotwell-publishing/flickr.png \
 	plugins/shotwell-publishing/flickr_publishing_options_pane.glade \
+	plugins/shotwell-publishing/flickr_pin_entry_pane.glade \
 	plugins/shotwell-publishing/picasa.png \
 	plugins/shotwell-publishing/picasa_publishing_options_pane.glade \
 	plugins/shotwell-publishing/youtube.png \
+	plugins/shotwell-publishing/youtube_publishing_options_pane.glade \
 	plugins/shotwell-publishing/piwigo.png \
 	plugins/shotwell-publishing/piwigo_authentication_pane.glade \
 	plugins/shotwell-publishing/piwigo_publishing_options_pane.glade \
