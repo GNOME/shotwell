62869fca9eb42d1fab2df037f31044ef5cf4e67b
Jens Georg <mail@jensge.org>
2018-03-12 22:50:32 +0100

    Fix undefined symbol error
    Or: inline does not do what you expect it to do.
    
    https://en.wikipedia.org/wiki/Inline_function#C99
    
    We really want static inline here.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=794293

--- src/_transformation.c
+++ src/_transformation.c
@@ -7,7 +7,7 @@
 
 #include "shotwell-graphics-processor.h"
 
-inline void _pixel_transformer_apply_transformations (PixelTransformer* self, RGBAnalyticPixel* p, RGBAnalyticPixel* result) {
+static inline void _pixel_transformer_apply_transformations (PixelTransformer* self, RGBAnalyticPixel* p, RGBAnalyticPixel* result) {
     PixelFormat current_format = PIXEL_FORMAT_RGB;
     RGBAnalyticPixel p_rgb = {p->red, p->green, p->blue };
     HSVAnalyticPixel p_hsv = {0.0f, 0.0f, 0.0f};
